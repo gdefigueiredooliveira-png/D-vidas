3813bf14134bb347e41f20be8397c30ccaeb6514
Gabriel <gdefigueiredooliveira@gmail.com>
2026-07-17 16:46:08 -0400

    Função adcionada e finalizada

--- fatorial.py
+++ fatorial.py
@@ -1,21 +1,27 @@
-while True:
-    try:
-        numero=int(input('Digite um número: '))
-        break
-    except:
-        print("+---------------------------------------------+")
-        print('|                                             |')
-        print('|             DIGITE APENAS NÚMEROS           |')
-        print('|                                             |')
-        print("+---------------------------------------------+")
+def calcular_fatorial():
+    while True:
+        try:
+            numero = int(input('Digite um número: '))
+            break
+        except:
+            print("+---------------------------------------------+")
+            print("|                                             |")
+            print("|             DIGITE APENAS NÚMEROS           |")
+            print("|                                             |")
+            print("+---------------------------------------------+")
 
-fatorial=1
-print(f'\nCálculo de {numero}:')
-for i in range (numero, 0, -1):
-    print(i , end="")
-    fatorial *= i
-    if i > 1:
-        print (' X ', end="")
-    else:
-        print(" = ",end="")
-print(fatorial,"\n")
+    fatorial = 1
+    print(f'\nCálculo de {numero}:')
+
+    for i in range(numero, 0, -1):
+        print(i, end="")
+        fatorial *= i
+
+        if i > 1:
+            print(" X ", end="")
+        else:
+            print(" = ", end="")
+
+    print(fatorial, "\n")
+
+calcular_fatorial()
